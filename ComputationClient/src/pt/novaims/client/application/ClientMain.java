ba35b0a720bf01a035510ac5f61daebf44febeff
Sakari <hassi.sakari.j@student.uta.fi>
2015-10-20 14:49:40 +0100

    Small fixes to test client

--- ComputationClient/src/pt/novaims/client/application/ClientMain.java
+++ ComputationClient/src/pt/novaims/client/application/ClientMain.java
@@ -10,35 +10,33 @@ import java.net.UnknownHostException;
 public class ClientMain {
 
 	public final static int PORT = 8888;
+	public final static String KEYWORD = "Computation";
 	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
 		
 		DatagramSocket datagramSocket;
-		
-			try {
-				datagramSocket = new DatagramSocket();
-				byte[] buffer = "Computation".getBytes();
-				InetAddress receiverAddress = InetAddress.getLocalHost();
-	
-				DatagramPacket packet = new DatagramPacket(
-				        buffer, buffer.length, InetAddress.getByName("255.255.255.255"), PORT);
-				datagramSocket.send(packet);
-			} catch (SocketException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				System.err.println("Unknown host "); 
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
+		//while(true) {
+		try {
+			datagramSocket = new DatagramSocket();
+			byte[] buffer = KEYWORD.getBytes();
 
-		
-
+			DatagramPacket packet = new DatagramPacket(
+			        buffer, buffer.length, InetAddress.getByName("255.255.255.255"), PORT);
+			datagramSocket.send(packet);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Unknown host "); 
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//}
 	}
 
 }
